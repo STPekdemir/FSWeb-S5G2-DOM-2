1303f8ea9e8ed107b8966696c9d53c3ec540b566
STPekdemir <119021180+STPekdemir@users.noreply.github.com>
2022-12-28 01:33:16 +0300

    load event

--- src/index.js
+++ src/index.js
@@ -4,22 +4,34 @@ import "./less/index.less";
 let time = 0;
 setInterval(function () {
   time += 1000;
-  if (time === 10000) {
+  if (time === 20000) {
     alert("Hala buradaysan bir tuşa bas!!!");
   }
 }, 1000);
 document.addEventListener("keydown", () => (time = 0));
-// sayfada 10 saniye hiçbir tuşa basmadan durulursa çalışır bir tuşa basıldığında sıfırlanır. Çalıştığında alert içerisinde bir string verir.
+// sayfada 20 saniye hiçbir tuşa basmadan durulursa çalışır bir tuşa basıldığında sıfırlanır. Çalıştığında alert içerisinde bir string verir.
 
 //wheel event
 let image = document.querySelectorAll("img");
 image.forEach(function (img) {
-  img.addEventListener("wheel", function (event) {
-    if (event.deltaY < 0) {
-      img.style.transform = "scale(2)";
-    } else {
-      img.style.transform = "scale(1)";
-    }
-  });
+  img.addEventListener(
+    "wheel",
+    function (event) {
+      if (event.deltaY < 0) {
+        img.style.transform = "scale(2)";
+      } else {
+        img.style.transform = "scale(1)";
+      }
+    },
+    true
+  );
 });
 // resilere zoom yapar.
+// load event
+window.addEventListener("load", function () {
+  document.body.style.backgroundColor = "red";
+  setTimeout(function () {
+    document.body.style.backgroundColor = "";
+  }, 5000);
+});
+//mouse over event
