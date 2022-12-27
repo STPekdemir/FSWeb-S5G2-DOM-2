09a6bfbfbdb883210718e41c85c0527d8ebf0142
STPekdemir <119021180+STPekdemir@users.noreply.github.com>
2022-12-27 20:47:16 +0300

    keydown

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-import './less/index.less'
+import "./less/index.less";
 
 // Örnek bir event kullanımı aşağıdadır. Çalıştırmak için comment dışına alın
 // document.querySelector("h1").addEventListener("click",function(e){
 // alert("Bana tıkladın!")
 // });
-
+// keydown event
+let time = 0;
+setInterval(function () {
+  time += 1000;
+  if (time === 10000) {
+    alert("Hala buradaysan bir tuşa bas!!!");
+  }
+}, 1000);
+document.addEventListener("keydown", () => (time = 0));
 
 // Kodlar buraya gelecek!
