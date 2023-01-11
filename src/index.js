dbe001d89b70a80563d1ede03b6d3ed096de52f9
STPekdemir <119021180+STPekdemir@users.noreply.github.com>
2023-01-11 08:02:00 +0300

    Focus select resize  wheel
    focus ve select için input elementi eklendi

--- src/index.js
+++ src/index.js
@@ -57,10 +57,32 @@ paragraf.forEach(function (p) {
 
 // paragraf elementleri üzerine gelindiğinde büyütür üzerinden çekilindiğinde normal haline geri döner
 
-//focus event
 //resize event
+window.addEventListener("resize", function () {
+  if (window.innerWidth < 800) {
+    document.querySelectorAll(".content-section").forEach(function (section) {
+      section.style.flexDirection = "column";
+    });
+  } else {
+    document.querySelectorAll(".content-section").forEach(function (section) {
+      section.style.flexDirection = "";
+    });
+  }
+});
+//800 width altına inilirse content section flexi column olacak
+
 //scroll event
-//select event
+document.addEventListener("wheel", function (event) {
+  let elements = document.querySelectorAll(
+    "p, h1, h2, h3, h4, h5, h6, span, a"
+  );
+  if (event.deltaY > 0) {
+    elements.forEach((el) => (el.style.color = "orange"));
+  } else {
+    elements.forEach((el) => (el.style.color = "black"));
+  }
+});
+//sayfayı aşşağı sürüklediğimizde tüm text renkleri turuncuya döner yukarı çevirdiğimizde siyaha döner
 
 //dblclick event
 
@@ -74,3 +96,20 @@ document.addEventListener("dblclick", () => {
 // arka planın 5 saniye sarı olmasını sağlar
 
 //drag / drop event
+//focus event
+let input = document.querySelector(".input");
+setTimeout(() => {
+  input.addEventListener("focus", () => {
+    input.select();
+  });
+  input.focus();
+}, 30000);
+// 30 saniye sonra isminiz input elementine focus yaparak text şeklinde nput gşrmemizi sağlar (eğer keydown eventi çalıştıysa alert kapatılana kadar focus eventi gerçekleşmez)
+//select  ilede eğer İsmini kısmına herhangi bir text girildiyse o texti otamatik olarak seçmemizi sağlar
+
+//select event
+const email = document.querySelector(".email");
+email.addEventListener("select", function () {
+  console.log("Emailinizi seçtiniz");
+});
+// Email inputu içerisindeki texti seçtiğinizde konsala "Emailinizi seçtiniz" yazdırır
